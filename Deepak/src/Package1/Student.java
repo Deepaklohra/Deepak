7e32aa3652fa59ac8da39705ce69b30e351fd803
HP <HP@LAPTOP-4735528P>
2023-09-09 10:00:41 +0530

    First commit

---
+++ Deepak/src/Package1/Student.java
@@ -0,0 +1,29 @@
+package Package1;
+
+public class Student 
+{
+	int age;
+	int rollno;
+
+   public void display1()
+   {
+	   System.out.println("Welcome to all of you");
+}	
+	
+   public void display2()
+   {
+	   System.out.println("Automation is very easy");   
+	}
+   public static void main(String[] args) {
+	
+	   Student ob=new Student();
+	   ob.age = 24;
+	   ob.rollno = 35;
+	   ob.display1();
+	   ob.display2();
+	   
+	   System.out.println("age is " + ob.age);
+	   System.out.println("rollno is " + ob.rollno);
+			   
+}
+	}	
